cae06b9621b9bce7f512b74bde6a82817744a641
yashweblife <yashweblife20@gmail.com>
2023-07-09 13:58:53 -0500

    add function defs

--- Vect3D.cpp
+++ Vect3D.cpp
@@ -7,12 +7,51 @@ Vect3D::Vect3D(float _x, float _y, float _z)
     y = _y;
     z = _z;
 }
-float Vect3D::getMag(){}
-void Vect3D::add(Vect3D &v) {}
-void Vect3D::sub(Vect3D &v) {}
-void Vect3D::mul(Vect3D &v) {}
-void Vect3D::scale(float v) {}
-void Vect3D::normalize() {}
-void Vect3D::rotateX(float angle) {}
-void Vect3D::rotateY(float angle) {}
-void Vect3D::rotateZ(float angle) {}
+float Vect3D::getMag()
+{
+    return (sqrt(x * x + y * y + z * z))
+}
+void Vect3D::add(Vect3D &v)
+{
+    x += v.x;
+    y += v.y;
+    z += v.z;
+}
+void Vect3D::sub(Vect3D &v)
+{
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+}
+void Vect3D::mul(Vect3D &v)
+{
+    x *= v.x;
+    y *= v.y;
+    z *= v.z;
+}
+void Vect3D::scale(float v)
+{
+    x *= v;
+    y *= v;
+    z *= v;
+}
+void Vect3D::normalize()
+{
+    float mag = sqrt(x * x + y * y + z * z);
+    x /= mag;
+    y /= mag;
+    z /= mag;
+}
+void Vect3D::rotateX(float angle) {
+    y = cos(angle)-sin(angle);
+    z = sin(angle)+cos(angle);
+
+}
+void Vect3D::rotateY(float angle) {
+    x = cos(angle) + sin(angle);
+    z = -sin(angle) + cos(angle);
+}
+void Vect3D::rotateZ(float angle) {
+    x = cos(angle) - sin(angle);
+    y = sin(angle) + cos(angle);
+}
